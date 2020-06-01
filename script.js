2a6e6968d462206d3b6f66bfee5d73730e430830
Robert Duhig <robertduhig@zoho.com>
2020-06-01 16:22:38 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/* ~~~~~~~~~~~~~~~~~~~~ MVP ~~~~~~~~~~~~~~~~~~~~ */
+
+// store all the plants as objects in an array
+
+// create an array to store the plants that are compatible with the user's selected garden conditions
+
+// when the user selects their sun exposure condition, traverse the plant array and add all the matching plants to the compatible array
+
+// when the user selects their soil type, traverse the compatible array and add all the matching plants to the new array
+
+// display the contents of the compatible array to the user
+
+// when the user selects their chosen plants with checkboxes, populate a div with the picture and name of that chosen plant
+
+// create a button to clear the user's garden conditions and displayed plants
+
+/* ~~~~~~~~~~~~~~~~~~~~ STRETCH GOALS ~~~~~~~~~~~~~~~~~~~~ */
+
+// create an array to store the plants that match the user's selected filters
+
+// when the user selects options to filter the plants, traverse the compatible array and add all the matching plants to the filtered array
+
+// display the contents of the filtered array to the user
+
+// create a button to clear the user's filters
+
+// for each plant that is displayed, create a div with a background color that matches the bloom of that plant, and overlay that div on the bloom timeline to represent that plant's bloom period
